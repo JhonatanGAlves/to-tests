b058254b0527da7d57a3c609b99f0312eee91459
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-05 11:49:15 -0300

    creating component Stylesheet

--- src/components/listsAndStyles/ListsAndStyles.tsx
+++ src/components/listsAndStyles/ListsAndStyles.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { StyledExerciseInput } from '../InputState'
 import StylesInLine from './StylesInLine'
+import Stylesheet from './Stylesheet'
 
 const ListsAndStyles = () => {
   return (
     <StyledExerciseInput>
       <StylesInLine />
+      <hr />
+      <Stylesheet />
     </StyledExerciseInput>
   )
 }
---
+++ src/components/listsAndStyles/Stylesheet.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Stylesheet.css'
+
+const Stylesheet = () => {
+  return (
+    <div className='Stylesheet'>
+      <h3>Stylesheet</h3>
+      <button className='btn'>
+        Stylesheet
+      </button>
+    </div>
+  )
+}
+
+export default Stylesheet
