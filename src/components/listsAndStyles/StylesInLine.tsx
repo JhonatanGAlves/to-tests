75a438eb770e95d2a0eaf1763d7a3080b2881833
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-05 11:11:10 -0300

    StylesInline: some forms of dynamic styles

--- src/components/listsAndStyles/StylesInLine.tsx
+++ src/components/listsAndStyles/StylesInLine.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StylesInLine = () => {
+  const [clicked, setClicked] = useState(false)
+  const [bgColor, setBgColor] = useState('#800000')
+
+  const handleClicked = () => {
+    if (!clicked) {
+      setClicked(true)
+      setBgColor('#B22222')
+    } else {
+      setClicked(false)
+      setBgColor('#800000')
+    }
+  }
+
   return (
     <div className='inLine'>
       <h3 style={{ marginBottom: '5px' }}>Styles inline</h3>
-      <button style={
-        {
-          backgroundColor: '#800000',
-          color: '#FFF',
+      <button
+        onClick={handleClicked}
+        style={
+          {
+            backgroundColor: bgColor,
+            color: '#FFF',
 
-          padding: '10px 20px',
-          border: '0',
-          borderRadius: '5px'
+            padding: '10px 20px',
+            border: '0',
+            borderRadius: clicked ? '5px 15px 5px 15px' : '0'
+          }
         }
-      }
       >Inline
       </button>
     </div>
