070a2fc50498eddafb1177efe57a57f69d5bc9d9
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-05 20:00:42 -0300

    StyledComponents: another way of use

--- src/components/listsAndStyles/StyledComponents.tsx
+++ src/components/listsAndStyles/StyledComponents.tsx
@@ -1,26 +1,17 @@
 import React from 'react'
-import styled from 'styled-components'
+import * as C from './StylesStyledComponents'
+
+// Uma das formas bastante utilizada é criar os styled components em um arquivo separado e depois somente importa-los
+// usando * as C, * para pegar todos os components daquele arquivo e C para chamar o componente, usando C. dessa forma
+// ira aparecer para selecionar o component que deseja usar.
 
 const StyledComponents = () => {
   return (
     <>
       <h3>Styled Components</h3>
-      <Button>Styled Components</Button>
+      <C.Button>Styled Components</C.Button>
     </>
   )
 }
-
-const Button = styled.button`
-  background-color:	#FF6347;
-  color: #FFF;
-
-  padding: 10px 20px;
-  border: 0;
-  border-radius: 15px 5px;
-
-  &:hover {
-    background-color: #FF0000;
-  }
-`
 
 export default StyledComponents
---
+++ src/components/listsAndStyles/StylesStyledComponents.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background-color:	#FF6347;
+  color: #FFF;
+
+  padding: 10px 20px;
+  border: 0;
+  border-radius: 15px 5px;
+
+  &:hover {
+    background-color: #FF0000;
+  }
+`
