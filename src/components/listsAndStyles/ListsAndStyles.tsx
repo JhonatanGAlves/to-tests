32cb23ea98acea9d0b9fba2a0d90d115a4f5f616
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-05 09:58:25 -0300

    creating container with all lists and styles

---
+++ src/components/listsAndStyles/ListsAndStyles.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledExerciseInput } from '../InputState'
+
+const ListsAndStyles = () => {
+  return (
+    <StyledExerciseInput>
+
+    </StyledExerciseInput>
+  )
+}
